f3b4c8f577c6dba49525f060710ddcf6ceeb7745
blackaslife <blackaslife2005@gmail.com>
2023-06-18 15:30:48 +0200

    change js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function showText (element) {
     if(element.previousElementSibling.clientHeight === 80) {
      element.previousElementSibling.style.height = "100%";
-     element.innerHtml = "Show less...";
+     element.innerHTML = "Show less...";
     } else {
         element.previousElementSibling.style.height ="80px";
-        element.innerHtml = "Read More..."
+        element.innerHTML = "Read More..."
     }
 }
